ee65ff91607a2a3b317814ae4a529987ff82edb4
Milki Dida <milkidida131@gmail.com>
2022-01-03 11:11:35 +0300

    Configure loggin and enviromental variables

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import logging
+
+from decouple import config
+
+# Configure logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+# Define Bot Token
+TOKEN = config("TOKEN")
