1de1e406de8c150a3bc9c8a5f85c95ab7d553bbd
Milki Dida <milkidida131@gmail.com>
2022-01-12 21:42:22 +0300

    Define update and dispatcher

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from decouple import config
+from telegram.ext import Updater
 
 # Configure logging
 logging.basicConfig(
@@ -11,3 +12,13 @@ logger = logging.getLogger(__name__)
 
 # Define Bot Token
 TOKEN = config("TOKEN")
+
+# Define Bot Updater
+updater = Updater(TOKEN)
+
+# Define Bot Dispatcher
+dispatcher = updater.dispatcher
+
+if __name__ == "__main__":
+    updater.start_polling()
+    updater.idle()
